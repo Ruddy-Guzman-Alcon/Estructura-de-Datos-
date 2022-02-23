bc50ef85949a72f7c75647a11600fc2b29d62838
Ruddy-Guzman-Alcon <78804914+Ruddy-Guzman-Alcon@users.noreply.github.com>
2022-02-23 11:08:45 -0400

    Create T#1 Vector promediar.h

---
+++ T#1 Vector promediar.h
@@ -0,0 +1,26 @@
+#pragma once
+#include"iostream"
+
+using namespace std;
+
+class Vector
+{
+	private:
+	////Atributos
+	int v[100];
+	int n;     ///Tamaño del vector
+	
+public:  ///Metodos
+
+	Vector(void);
+	~Vector(void);
+
+	///Metodos
+	void cargarVector(int v[], int n);
+	void mostrarVector(int v[], int n);
+	void promedio(int v[], int n);
+
+
+	
+	
+}
