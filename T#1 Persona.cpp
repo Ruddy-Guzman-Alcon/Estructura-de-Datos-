a4aa758a0b70334beff35ef83708cb1866572b83
Ruddy-Guzman-Alcon <78804914+Ruddy-Guzman-Alcon@users.noreply.github.com>
2022-02-23 10:56:31 -0400

    Create T#1 Persona.cpp

---
+++ T#1 Persona.cpp
@@ -0,0 +1,19 @@
+#include "StdAfx.h"
+#include "Persona.h"
+#include <conio.h>
+#include <iostream>
+
+using namespace std;
+
+Persona::Persona(string _nombre, int _edad)
+{
+	nombre= _nombre;
+	edad= _edad;
+}
+
+
+void Persona::mostrarPersona()
+{
+	cout<<"Nombre: "<<nombre<<endl;
+	cout<<"Edad: "<<edad<<endl;
+}
