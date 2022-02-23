99689e3a2e22dce891c9875f5f8546cc32c053b9
Ruddy-Guzman-Alcon <78804914+Ruddy-Guzman-Alcon@users.noreply.github.com>
2022-02-23 11:16:51 -0400

    Update T#1 Vector menor.cpp

--- T#1 Vector menor.cpp
+++ T#1 Vector menor.cpp
@@ -27,8 +27,22 @@ Vector::~Vector(void)
 		for(int i=0;i<n;i++)
 		{
 			cout<<v[i]<<", "<<endl;
+			 }
 
+void Vector::menor(int v[],int n)
+	{
+		int menor=9999;
+		for(int i=0;i<n;i++)
+		{
+			cout<<"ingrese un numeero: "<<endl;
+		    cin>>v[i];
+		    if(v[i]<menor)
+		    {
+			    menor=v[i];
+			
 
 		}
-		cout<<endl;
+		cout<<"el numero menor es : "<<menor<<endl;
+
 	}
+	
